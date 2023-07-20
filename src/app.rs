407f41e8accff56adc22db8acb8edab5b5001d7c
Kelvin César de Andrade <kelvincandrade@gmail.com>
2023-07-19 22:08:44 -0300

    init commit

---
+++ src/app.rs
@@ -0,0 +1,21 @@
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App(cx: Scope) -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context(cx);
+
+    view! { cx,
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
+
+        // sets the document title
+        <Title text="Welcome to LLAMA chat"/>
+          
+    }
+}
+
+
